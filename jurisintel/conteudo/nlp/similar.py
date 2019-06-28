d03ea66efa8abdf0e05c9304f71620bcb97479d6
Carlos <caique.blackhawk14@gmail.com>
2019-06-28 14:56:59 -0300

    Change of method to cosine (speed test)

--- jurisintel/conteudo/nlp/similar.py
+++ jurisintel/conteudo/nlp/similar.py
@@ -66,7 +66,7 @@ def similar(arquivo_referencia, lista_pets_escritorio):
 def similar_resumo(resumo_referencia, lista_resumos_escritorio):
     referencia = nlp(resumo_referencia)
     for ref in lista_resumos_escritorio:
-        yield(lista_resumos_escritorio.index(ref), similarity.word_movers(referencia, nlp(ref), metric='canberra'))
+        yield(lista_resumos_escritorio.index(ref), similarity.word_movers(referencia, nlp(ref), metric='cosine'))
 
 
 def similar_tags(resumo_ref, tag_list):
